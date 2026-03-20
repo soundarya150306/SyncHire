789c4e932cbad1eea00a3954ee35c95d80244351
SOUNDARYA <soundaryaram2006@gmail.com>
2026-03-20 23:37:25 +0530

    Revert GitHub Pages config, preparing for Vercel

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import { AuthProvider } from './context/AuthContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <BrowserRouter basename="/SyncHire">
+        <BrowserRouter>
             <AuthProvider>
                 <App />
             </AuthProvider>
